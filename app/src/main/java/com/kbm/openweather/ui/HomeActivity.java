8f74e842676762ac07b1643087921162085ea96e
Bassem Samy <bassem.samy@hotmail.com>
2017-07-27 00:06:06 +0200

    UI changes

--- app/src/main/java/com/kbm/openweather/ui/HomeActivity.java
+++ app/src/main/java/com/kbm/openweather/ui/HomeActivity.java
@@ -82,18 +82,9 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_current_weather) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_next_five_days) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
